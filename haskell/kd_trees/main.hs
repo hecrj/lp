4dad434ebb0495204f81d6b13e5b0e4b5a98e9d0
Héctor Ramón Jiménez <hector0193@gmail.com>
2014-05-09 01:17:50 +0200

    inserting and building trees

--- haskell/kd_trees/main.hs
+++ haskell/kd_trees/main.hs
@@ -1,79 +1,99 @@
+-- Useful functions
+-- variant of map that passes each element's index as second argument to f
+each :: (a -> Int -> b) -> [a] -> [b]
+each f l = zipWith f l [0..]
+
+
+-- Type class that represents a Point
 class Point p where
-    sel :: Int -> p -> Double
-    dim :: p -> Int    
-    listToPoint :: [Double] -> p
+  sel :: Int -> p -> Double
+  dim :: p -> Int
+  listToPoint :: [Double] -> p
 
-    child :: p -> p -> [Int] -> Int
-    child e1 e2 coords = child' e1 e2 coords 0 where
-        child' :: Point p => p -> p -> [Int] -> Int -> Int
-        child' _ _ [] result = result `div` 2
-        child' e1 e2 (x:xs) current = if sel x e1 <= sel x e2
-            then child' e1 e2 xs (current*2)
-            else child' e1 e2 xs ((current+1)*2)
+  child :: p -> p -> [Int] -> Int
+  child e1 e2 coords = (foldl (child' e1 e2) 0 coords) `div` 2 where
+    child' e1 e2 result x = if sel x e1 <= sel x e2
+      then result*2
+      else (result+1)*2
 
-    dist :: p -> p -> Double
-    dist e1 e2 = sqrt (sum diffComponents) where
-        diffComponents = [(sel i e2 - sel i e1)^2 | i <- [1..dim e1]]
+  dist :: p -> p -> Double
+  dist e1 e2 = sqrt $ sum diffComponents where
+      diffComponents = [(sel i e2 - sel i e1)^2 | i <- [1..dim e1]]
 
-    components :: p -> [Double]
-    components p = [(sel i p) | i <- [1..dim p]]
+  components :: p -> [Double]
+  components p = [(sel i p) | i <- [1..dim p]]
 
-    pointToString :: p -> String
-    pointToString p = "(" ++ intercalate "," (components p) ++ ")" where
-        intercalate :: String -> [Double] -> String
-        intercalate delim [] = ""
-        intercalate delim [x] = show x
-        intercalate delim (x:xs) = show x ++ delim ++ intercalate delim xs
+  pointToString :: p -> String
+  pointToString p = "(" ++ intercalate "," (components p) ++ ")" where
+    intercalate delim [] = ""
+    intercalate delim [x] = show x
+    intercalate delim (x:xs) = show x ++ delim ++ intercalate delim xs
 
-    pointEquals :: p -> p -> Bool
-    pointEquals e1 e2 = components e1 == components e2
+  pointEquals :: p -> p -> Bool
+  pointEquals e1 e2 = components e1 == components e2
 
+
+-- Point3d definition
 data Point3d = Point3d { x :: Double, y :: Double, z :: Double }
 
 instance Point Point3d where
-    sel 1 p = x p
-    sel 2 p = y p
-    sel 3 p = z p
+  sel 1 p = x p
+  sel 2 p = y p
+  sel 3 p = z p
 
-    dim p = 3
+  dim p = 3
 
-    listToPoint [x, y, z] = Point3d x y z
-
+  listToPoint [x, y, z] = Point3d x y z
 
 instance Show Point3d where
-    show p = pointToString p
+  show p = pointToString p
 
 instance Eq Point3d where
-    e1 == e2 = pointEquals e1 e2
+  e1 == e2 = pointEquals e1 e2
 
 
+-- Kd2nTree definition
 data Kd2nTree p = Empty | Node { point :: p, list :: [Int], children :: [Kd2nTree p] }
 
 instance (Show p) => Show (Kd2nTree p) where
-    show Empty = ""
-    show node = showNode node 0 where
-        showNode (Node point list children) level = show point ++ " " ++
-            show list ++ "\n" ++ showChildren children (level+1) 0 where
-                showChildren [] _ _ = ""
-                showChildren (x:xs) level current = showChild x level current ++
-                    showChildren xs level (current+1) where
-                        showChild Empty _ _ = ""
-                        showChild node level current = (replicate (level*4) ' ') ++
-                            "<" ++ show current ++ "> " ++ showNode x level
+  show Empty = ""
+  show node = show' node 0 where
+    show' node level = nodeStr ++ "\n" ++ childrenStr where
+      nodeStr = show (point node) ++ " " ++ show (list node)
+      childrenStr = foldr (++) "" (each (showChild (level+1)) (children node)) where
+        showChild _ Empty _ = ""
+        showChild level node current = indent ++ index ++ show' node level where
+          indent = replicate (level*4) ' '
+          index  = "<" ++ show current ++ "> "
 
 
+-- Kd2nTree functions
+insert :: Point p => Kd2nTree p -> p -> [Int] -> Kd2nTree p
+insert Empty p l = Node p l (take (2^(length l)) (repeat Empty))
+insert (Node point list children) p l = Node point list newChildren where
+  newChildren = each (insert' p l (child p point list)) children where
+    insert' p l selected child current = if selected == current
+      then insert child p l
+      else child
+
+build :: Point p => [(p, [Int])] -> Kd2nTree p
+build nodes = foldl (\t (p, l) -> insert t p l) Empty nodes
+
+buildIni :: Point p => [([Double], [Int])] -> Kd2nTree p
+buildIni nodes = foldl (\t (d, l) -> insert t (listToPoint d) l) Empty nodes
+
+
+-- Example set
 exampleSet :: Kd2nTree Point3d
-exampleSet = (Node (Point3d 3 (-1) 2.1) [1,3] [
-        (Node (Point3d 3 5.1 0) [2] [
-            (Node (Point3d 1.8 1.1 (-2)) [1,2] []),
-            (Node (Point3d 1.5 8 1.5) [1] [])
-        ]),
-        (Node (Point3d 3 (-1.7) 3.1) [1,2,3] []),
-        (Node (Point3d 3.5 0 2.1) [3] []),
-        (Node (Point3d 3.5 2.8 3.1) [1,2] [
-            (Node (Point3d 3.3 2.8 2.5) [3] []),
-            (Node (Point3d 3.1 3.8 4.8) [1,3] []),
-            Empty,
-            (Node (Point3d 4.0 5.1 3.8) [2] [])
-        ])
-    ])
+exampleSet = buildIni [
+    ([3.0, -1.0, 2.1], [1, 3]),
+    ([3.5, 2.8, 3.1], [1, 2]),
+    ([3.5, 0.0, 2.1], [3]),
+    ([3.0, -1.7, 3.1], [1, 2, 3]),
+    ([3.0, 5.1, 0.0], [2]),
+    ([1.5, 8.0, 1.5], [1]),
+    ([3.3, 2.8, 2.5], [3]),
+    ([4.0, 5.1, 3.8], [2]),
+    ([3.1, 3.8, 4.8], [1, 3]),
+    ([1.8, 1.1, -2.0], [1, 2])
+  ]
