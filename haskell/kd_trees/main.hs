9438522599368e6b054ea0fd1161cda3ee67f473
Héctor Ramón Jiménez <hector0193@gmail.com>
2014-05-09 18:50:32 +0200

    Finished! :D

--- haskell/kd_trees/main.hs
+++ haskell/kd_trees/main.hs
@@ -1,4 +1,5 @@
 -- Useful functions
+-- intercalates the first argument with the elements of the second argument
 intercalate :: Show s => String -> [s] -> String
 intercalate delim [] = ""
 intercalate delim [x] = show x
@@ -8,12 +9,23 @@ intercalate delim (x:xs) = show x ++ delim ++ intercalate delim xs
 each :: (Int -> a -> b) -> [a] -> [b]
 each f = zipWith f [0..]
 
+-- applies the first argument to the selected element and the second argument
+-- to the others
 mapSelect :: (a -> b) -> (a -> b) -> Int -> [a] -> [b]
 mapSelect f1 f2 selected = each (ifSelected f1 f2 selected)
   where
     ifSelected f1 f2 selected current
       | selected == current = f1
       | otherwise = f2
+
+-- obtains the minimum of the second argument given the min function
+minimumBy :: Ord b => (a -> b) -> [a] -> a
+minimumBy f (x:xs) = minimumBy' f xs x $ f x
+  where
+    minimumBy' f [] min _ = min
+    minimumBy' f (x:xs) min val
+      | f x < val = minimumBy' f xs x $ f x
+      | otherwise = minimumBy' f xs min val
 
 
 -- Type class that represents a Point
@@ -29,19 +41,25 @@ class Point p where
         | sel x e1 <= sel x e2 = result*2
         | otherwise = (result+1)*2
 
+  components :: p -> [Double]
+  components p = [(sel i p) | i <- [1..dim p]]
+
   dist :: p -> p -> Double
   dist e1 e2 = sqrt $ sum diffComponents
     where
-      diffComponents = [(sel i e2 - sel i e1)^2 | i <- [1..dim e1]]
-
-  components :: p -> [Double]
-  components p = [(sel i p) | i <- [1..dim p]]
+      diffComponents = zipWith (\a b -> (a - b)^2) (components e2) (components e1)
 
   pointToString :: p -> String
   pointToString p = "(" ++ intercalate "," (components p) ++ ")"
 
   pointEquals :: p -> p -> Bool
   pointEquals e1 e2 = components e1 == components e2
+
+  ptrans :: [Double] -> p -> p
+  ptrans t p = listToPoint $ zipWith (+) t $ components p
+
+  pscale :: Double -> p -> p
+  pscale s p = listToPoint $ map (*s) $ components p
 
 
 -- Point3d definition
@@ -100,15 +118,38 @@ buildIni = foldl (\t (d, l) -> insert t (listToPoint d) l) Empty
 
 get_all :: Kd2nTree p -> [(p, [Int])]
 get_all Empty = []
-get_all (Node point list children) = (point, list) : foldl (++) [] (map get_all children)
+get_all (Node point list children) = (point, list) : foldr (++) [] (map get_all children)
 
 remove :: (Point p, Eq p) => Kd2nTree p -> p -> Kd2nTree p
-remove tree point = build $ remove' point $ get_all tree
-  where
-    remove' point [] = []
-    remove' point ((x,l):xs)
-      | point == x = xs
-      | otherwise = (x,l) : remove' point xs
+remove Empty _ = Empty
+remove node@(Node np nl nc) point
+  | point == np = build $ tail $ get_all node
+  | otherwise = Node np nl newChildren
+      where
+        newChildren = mapSelect (remove' point) id selected nc
+          where
+            remove' point node = remove node point
+            selected = child point np nl
+
+contains :: (Point p, Eq p) => Kd2nTree p -> p -> Bool
+contains Empty _ = False
+contains (Node np nl nc) point
+  | point == np = True
+  | otherwise = contains (nc!!(child point np nl)) point
+
+nearest :: Point p => Kd2nTree p -> p -> p
+nearest tree point = minimumBy (dist point) $ map fst $ get_all tree
+
+kdmap :: (p -> q) -> Kd2nTree p -> Kd2nTree q
+kdmap _ Empty = Empty
+kdmap f (Node point list children) = Node (f point) list $ map (kdmap f) children
+
+translation :: Point p => [Double] -> Kd2nTree p -> Kd2nTree p
+translation t = kdmap $ ptrans t
+
+scale :: Point p => Double -> Kd2nTree p -> Kd2nTree p
+scale s = kdmap $ pscale s
+
 
 -- Example set
 exampleSet :: Kd2nTree Point3d
