dd680530ca55882684c990f2de7f008179750bcb
Héctor Ramón Jiménez <hector0193@gmail.com>
2014-05-17 21:25:23 +0200

    General cleaning.

--- haskell/soskell/soskell.hs
+++ haskell/soskell/soskell.hs
@@ -18,6 +18,9 @@ intercalate delim [] = ""
 intercalate delim [x] = show x
 intercalate delim (x:xs) = show x ++ delim ++ intercalate delim xs
 
+join :: String -> [String] -> String
+join glue [x,y] = x ++ glue ++ y
+
 separate :: [String] -> String
 separate [] = ""
 separate [x] = x
@@ -53,43 +56,53 @@ selectOption options = do
 
 
 -- Game types
-data GameType = UserVsCPU | CPUvsCPU
-
-gameTypes :: [GameType]
-gameTypes = [UserVsCPU, CPUvsCPU]
+data GameType = GameType { description :: String, factory :: GameFactory }
+type GameFactory = Board -> IO Game
 
 instance Show GameType where
-  show UserVsCPU = "User vs CPU"
-  show CPUvsCPU = "CPU vs CPU"
+  show = description
 
+gameTypes :: [GameType]
+gameTypes = [
+    GameType "User vs CPU" createUserVsCPU,
+    GameType "CPU vs CPU" createCPUVsCPU
+  ]
+
+createUserVsCPU :: GameFactory
+createUserVsCPU board = do
+  putStrLn "Select the strategy of the CPU player:"
+  cpu <- selectOption cpuPlayers
+  putStrLn "Who will make the first move?"
+  let player = Player "User" user 0
+  putOptions [player, cpu]
+  firstMove <- promptInt "Select option" 1 2
+  return (Game player cpu board (firstMove-1) 1)
+
+createCPUVsCPU :: GameFactory
+createCPUVsCPU board = do
+  putStrLn "Select the strategy of the first player:"
+  player1 <- selectOption cpuPlayers
+  putStrLn "Select the strategy of the second player:"
+  player2 <- selectOption cpuPlayers
+  return (Game player1 player2 board 0 1)
 
 -- Game
 data Game = Game { player1 :: Player, player2 :: Player, board :: Board,
   nowPlays :: Int, turn :: Int }
 
-create :: GameType -> Board -> IO Game
-
-create UserVsCPU board = do
-  putStrLn "Select the strategy of the CPU player:"
-  cpu <- selectOption cpuStrategies
-  putStrLn "Who will make the first move?"
-  putOptions [User, cpu]
-  firstMove <- promptInt "Select option" 1 2
-  return (Game (Player User 0) (Player cpu 0) board (firstMove-1) 1)
-
-create CPUvsCPU board = do
-  putStrLn "Select the strategy of the first player:"
-  strat1 <- selectOption cpuStrategies
-  putStrLn "Select the strategy of the second player:"
-  strat2 <- selectOption cpuStrategies
-  return (Game (Player strat1 0) (Player strat2 0) board 1 1)
+instance Show Game where
+  show (Game p1 p2 board _ turn) = scoreStr ++ "\n" ++ boardStr
+    where
+      scoreStr = join "  -  " $ map showWithScore [p1, p2]
+      showWithScore = (\p -> (show p) ++ " " ++ (show $ score p))
+      boardStr = show board
 
 play :: Game -> IO ()
 play game@(Game player1 player2 board nowPlays turn)
   | turn > size board = finished game
   | otherwise = do
     let player = [player1, player2]!!nowPlays
-    action <- getAction game (strategy player)
+    action <- (strategy player) game
     play $ update game action
 
 update :: Game -> Action -> Game
@@ -100,26 +113,30 @@ update game@(Game p1 p2 board plays turn) action
     nextGame = (Game newPlayer1 newPlayer2 newBoard newPlays (turn+1))
     (newBoard, scored) = apply board action
     [newPlayer1, newPlayer2] = mapSelect
-      (\(Player strat score) -> Player strat (score+scored)) plays [p1, p2]
+      (\(Player name strat score) -> Player name strat (score+scored)) plays [p1, p2]
     newPlays
       | scored > 0 = plays
       | otherwise = (plays+1) `mod` 2
 
 finished :: Game -> IO ()
-finished (Game p1 p2 board _ _) = do
-  draw board
+finished game = do
+  putStrLn $ show game
   putStrLn "Game finished!"
 
 
 -- Board
+data Board = Board { rows :: Int, cols :: Int, cells :: [[Cell]]}
 data Cell = Empty | S | O deriving Eq
+
+instance Show Board where
+  show (Board _ _ cells) = separate rows
+    where
+      rows = map ((\x -> " " ++ x ++ " ") . (intercalate " | ")) cells
 
 instance Show Cell where
   show Empty = " "
   show S = "S"
   show O = "O"
-
-data Board = Board { rows :: Int, cols :: Int, content :: [[Cell]]}
 
 readBoard :: IO Board
 readBoard = do
@@ -133,55 +150,45 @@ newBoard rows cols = Board rows cols [take cols $ repeat Empty | _ <- [1..rows]]
 size :: Board -> Int
 size (Board rows cols _) = rows * cols
 
-draw :: Board -> IO ()
-draw (Board _ _ content) = drawContent content
-
-drawContent :: [[Cell]] -> IO ()
-drawContent content = do
-  putStrLn $ separate $ map ((\x -> " " ++ x ++ " ") . (intercalate " | ")) content
-
 getCell :: Board -> Int -> Int -> Cell
-getCell board row = (!!) ((content board)!!row)
+getCell board row = (!!) ((cells board)!!row)
 
 isValid :: Board -> Action -> Bool
 isValid board (Action row col _) = getCell board row col == Empty
 
 apply :: Board -> Action -> (Board, Int)
-apply (Board rows cols content) (Action row col letter) = (newBoard, scored)
+apply (Board rows cols cells) (Action row col letter) = (newBoard, scored)
   where
     newBoard = Board rows cols updatedCells
     scored = 0
-    updatedCells = mapSelect (mapSelect (\_ -> letter) col) row content
+    updatedCells = mapSelect (mapSelect (\_ -> letter) col) row cells
 
 
 -- Strategies
-data Strategy = User | Random
-data Player = Player { strategy :: Strategy, score :: Int }
+type Strategy = Game -> IO Action
+data Player = Player { name :: String, strategy :: Strategy, score :: Int }
 data Action = Action { row :: Int, col :: Int, letter :: Cell }
 
-instance Show Strategy where
-  show User = "User"
-  show Random = "Random"
-
 instance Show Player where
-  show (Player strat score) = show strat ++ " " ++ show score
+  show = name
 
-cpuStrategies :: [Strategy]
-cpuStrategies = [Random]
+cpuPlayers :: [Player]
+cpuPlayers = [
+    Player "Random" rand 0
+  ]
 
-getAction :: Game -> Strategy -> IO Action
-
-getAction game@(Game player1 player2 board _ turn) User = do
-  putStrLn $ "Turn " ++ (show turn)
-  putStrLn $ (show player1) ++ "  -  " ++ (show player2)
-  draw board
+user :: Strategy
+user game@(Game player1 player2 board _ turn) = do
+  putStrLn $ show turn
+  putStrLn $ show game
   row <- promptInt "Enter the row you want to modify" 1 $ rows board
   col <- promptInt "Enter the column yo want to modify" 1 $ cols board
   putStrLn "Select the letter you want to add:"
   letter <- selectOption [S, O]
   return $ Action (row-1) (col-1) letter
 
-getAction (Game _ _ board _ _) Random = do
+rand :: Strategy
+rand (Game _ _ board _ _) = do
   row <- randomRIO (0, (rows board)-1)
   col <- randomRIO (0, (cols board)-1)
   letter <- randomRIO (0, 1)
@@ -199,5 +206,5 @@ main = do
 
   gameType <- selectOption gameTypes
   board <- readBoard
-  game <- create gameType board
+  game <- (factory gameType) board
   play game
